a7437872949b1cb0550cc0b9a111c9ce283e4a8e
JgeovanniAm <geovanniam171078@gmail.com>
2019-09-25 00:16:37 -0600

    update: styles scss

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,16 +1,9 @@
 (function navegation() {
-  const btnOpen = document.querySelector('#open');
-  const body = document.querySelector('body');
+  const btnOpen = document.querySelector('.btn-nav');
   const nav = document.querySelector('.wrapper-nav');
-  const btnClose = document.querySelector('#close');
   function openNav() {
-    nav.classList = 'nav-open';
-    body.style.overflow = 'hidden';
-  }
-  function closeNav() {
-    nav.className = 'wrapper-nav';
-    body.style.overflow = 'initial';
+    nav.classList.toggle('nav-open');
+    btnOpen.classList.toggle('btn-nav--close');
   }
   btnOpen.addEventListener('click', openNav);
-  btnClose.addEventListener('click', closeNav);
 }());
