dda8fa0c529660024e1d7f2cd34138ec431f78e5
JgeovanniAm <geovanniam171078@gmail.com>
2019-09-26 21:39:26 -0600

    update: text-animation and btn-nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,9 +1,20 @@
 (function navegation() {
   const btnOpen = document.querySelector('.btn-nav');
   const nav = document.querySelector('.wrapper-nav');
+  const body = document.querySelector('body');
+  const text_btn = document.querySelector('.btn-nav__text');
+  let state = true;
   function openNav() {
     nav.classList.toggle('nav-open');
-    btnOpen.classList.toggle('btn-nav--close');
+    if (state == true){ 
+      text_btn.innerHTML = 'cerrar';
+      body.style.overflow = 'hidden';
+      state = false;
+    }else if (state == false) {
+      text_btn.innerHTML = 'menú';
+      body.style.overflow = 'initial';
+      state = true;
+    }
   }
   btnOpen.addEventListener('click', openNav);
 }());
